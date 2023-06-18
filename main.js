3564aaa731a007414981138d037fd524cdaacad8
boralex <boris.alexandre.rose@gmail.com>
2023-06-18 18:26:52 +0200

    "feat/responsive"

--- main.js
+++ main.js
@@ -1,18 +1,18 @@
 console.log('main.js is on')
-
-
+const showButton = document.querySelector('#home-section__button');
 /* declaring a function */
 /** =>  1 arrow function */
 const showProducts = () => {
 
     console.log('showProducts is on ...');
 
+    showButton.innerHTML = "En cours de chargement ..."
 
     setTimeout(() => {
 
         window.location = './views/products.html'
 
-    }, 5000)
+    }, 3000)
  
 }
 
